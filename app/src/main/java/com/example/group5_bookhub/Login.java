b1a8e59a6970db4ccc26a66346dc6f863aadabc5
gMethmini <gayali.9812@gmail.com>
2024-03-06 13:05:35 -0800

    feat:added the user profile screen

--- app/src/main/java/com/example/group5_bookhub/Login.java
+++ app/src/main/java/com/example/group5_bookhub/Login.java
@@ -2,7 +2,10 @@ package com.example.group5_bookhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button loginButton = findViewById(R.id.btnSignIn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, MainMenu.class));
+            }
+        });
     }
 }
