1b31bb5d14e824be556f939468b75bbb24d40de3
Aaron Omale <omalea@student.douglascollege.ca>
2024-03-10 14:44:40 -0700

    Fix: change color for login, splash, register and main_menu activities

--- app/src/main/java/com/example/group5_bookhub/Login.java
+++ app/src/main/java/com/example/group5_bookhub/Login.java
@@ -2,7 +2,9 @@ package com.example.group5_bookhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView signUpText = findViewById(R.id.tvLoginSignUp);
+
+        // Sign up click
+        signUpText.setOnClickListener(v -> startActivity(new Intent(Login.this, Register.class)));
     }
 }
--- app/src/main/java/com/example/group5_bookhub/Register.java
+++ app/src/main/java/com/example/group5_bookhub/Register.java
@@ -2,7 +2,9 @@ package com.example.group5_bookhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView textSignUp = findViewById(R.id.tvSignUp);
+
+        textSignUp.setOnClickListener(v -> startActivity(new Intent(Register.this, Login.class)));
     }
 }
