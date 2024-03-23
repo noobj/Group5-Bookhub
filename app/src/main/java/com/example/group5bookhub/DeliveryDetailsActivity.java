00ab8556076f1155a578ea8a19180303f3c7ad21
aanutaliya <anan.anutaliya@gmail.com>
2024-03-23 14:57:21 -0700

    feat: add logic insert book

--- app/src/main/java/com/example/group5bookhub/DeliveryDetailsActivity.java
+++ app/src/main/java/com/example/group5bookhub/DeliveryDetailsActivity.java
@@ -1,10 +1,17 @@
 package com.example.group5bookhub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class DeliveryDetailsActivity extends AppCompatActivity {
 
@@ -19,7 +26,7 @@ public class DeliveryDetailsActivity extends AppCompatActivity {
         order.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(DeliveryDetailsActivity.this, "Order placed successfully", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -27,6 +34,34 @@ public class DeliveryDetailsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+
+        // Bottom navigation
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+//         Perform item reselected listener
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int selectedId = menuItem.getItemId();
+
+                if (selectedId == R.id.buy) {
+                    // Handle "Buy" selection
+//                    startActivity(new Intent(BookDetailsActivity.this, MainActivity.class));
+                    startActivity(new Intent(DeliveryDetailsActivity.this, BuyBookActivity.class));
+                } else if (selectedId == R.id.sell) {
+                    // Handle "Sell" selection
+                    startActivity(new Intent(DeliveryDetailsActivity.this, SellBookActivity.class));
+                } else if (selectedId == R.id.order) {
+                    // Handle "Order" selection
+                    startActivity(new Intent(DeliveryDetailsActivity.this, OrderHistoryActivity.class));
+                } else if (selectedId == R.id.profile) {
+                    // Handle profile selection
+                    startActivity(new Intent(DeliveryDetailsActivity.this, UserProfileActivity.class));
+                }
+                return true;
             }
         });
     }
--- app/src/main/java/com/example/group5bookhub/SellBookActivity.java
+++ app/src/main/java/com/example/group5bookhub/SellBookActivity.java
@@ -6,25 +6,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class SellBookActivity extends AppCompatActivity {
 
+    EditText titleBook;
+    EditText description;
+    EditText price;
+    EditText authorBook;
+    RadioButton sellBook;
+    RadioButton shareBook;
+    Button addBtn;
+    DatabaseHelper databaseHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_book);
 
-//        EditText titile = findViewById(R.id.edTitle);
-//        EditText des = findViewById(R.id.edDescription);
-//        EditText price = findViewById(R.id.edPrice);
-//        Spinner categ = findViewById(R.id.spinnerCategory);
-//        RadioButton sell = findViewById(R.id.rdForSale);
-//        RadioButton share = findViewById(R.id.rdShare);
-//        Button uploadImg = findViewById(R.id.btnUploadImg);
-//        Button addBook = findViewById(R.id.btnAddBook);
+        titleBook = findViewById(R.id.edTitle);
+        description = findViewById(R.id.edDescription);
+        price = findViewById(R.id.edPrice);
+        authorBook = findViewById(R.id.edAuthor);
+        sellBook = findViewById(R.id.rdForSale);
+        shareBook = findViewById(R.id.rdShare);
+        addBtn = findViewById(R.id.btnAddBook);
+        databaseHelper = new DatabaseHelper(this);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = titleBook.getText().toString().trim();
+                String des = description.getText().toString().trim();
+                String author = authorBook.getText().toString().trim();
+                String priceStr = price.getText().toString().trim();
+                boolean isForSale = sellBook.isChecked();
+
+                //check for empty fields
+                if(title.isEmpty() || des.isEmpty() || author.isEmpty() || priceStr.isEmpty()) {
+                    Toast.makeText(SellBookActivity.this, "Please fill all fields", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                float price = Float.parseFloat(priceStr);
+                int sellerId = 1;
+
+                boolean isInserted = databaseHelper.insertBook(title, author, des, price, isForSale, sellerId);
+
+                if (isInserted) {
+                    Toast.makeText(SellBookActivity.this, "Book added successfully", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(SellBookActivity.this, "Failed to add book", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         // Bottom navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
