5d8f3ffa53ee6233315b245f7b4a5b48d675a12a
noobj <jingfu99@gmail.com>
2024-03-30 01:31:27 -0700

    feat: add order table

--- app/src/main/java/com/example/group5bookhub/DatabaseHelper.java
+++ app/src/main/java/com/example/group5bookhub/DatabaseHelper.java
@@ -5,13 +5,17 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.Random;
 
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     final static String DATABASE_NAME ="Bookhub.db";
-    final static int DATABASE_VERSION = 4;
+    final static int DATABASE_VERSION = 6;
     final static String TABLE1_NAME = "User";
     final static String T1COL1 = "ID";
     final static String T1COL2 = "Username";
@@ -28,8 +32,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     final static String BOOK_PRICE = "Price";
     final static String BOOK_FOR_SALE = "IsForSale";
     final static String BOOK_SELLER = "SellerId";
-
     final static String BOOK_IMAGE = "Image";
+
+
+    final static String TABLE_ORDER = "Orders";
+    final static String ORDER_ID = "ID";
+    final static String ORDER_BOOK = "BookId";
+    final static String ORDER_SELLER = "SellerId";
+    final static String ORDER_BUYER = "BuyerId";
+    final static String ORDER_DATE = "Date";
+    final static String ORDER_ADDRESS = "Address";
+    final static String ORDER_STATUS = "Status";
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         SQLiteDatabase db = this.getWritableDatabase();
@@ -57,66 +71,53 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 + BOOK_IMAGE + " TEXT,"
                 + " FOREIGN KEY ("+BOOK_SELLER+") REFERENCES "+TABLE1_NAME+"("+T1COL1+"));";
         sqLiteDatabase.execSQL(query);
-//        query = "Insert into " + TABLE1_NAME
-//                + "("
-//                + T1COL2 + ","
-//                + T1COL3 + ","
-//                + T1COL4 + ","
-//                + T1COL5 + ") Values (" +
-//                "'admin',12345,'admin@gmail.com','Douglas College')";
-//
-//        sqLiteDatabase.execSQL(query);
-//
-//
-//
-//        query = "Insert into " + TABLE_BOOK
-//                + "("
-//                + BOOK_AUTHOR + ","
-//                + BOOK_DESC + ","
-//                + BOOK_SELLER + ","
-//                + BOOK_PRICE + ","
-//                + BOOK_TITLE + ","
-//                + BOOK_FOR_SALE + ","
-//                + BOOK_IMAGE +
-//                ") Values (" +
-//                "'Tony Faggioli','As our story unfolds, it becomes clear that there is hell, and then there is hell on earth.',1,18.99,'A Million to One: Beyond'," +
-//                "1, 'bookcover2'),('Jennifer L. Armentrout','Samantha is a stranger in her own life.',1,8.99,'Don''t Look Back',1, 'bookcover1')";
-//
-//        sqLiteDatabase.execSQL(query);
+
+        query = "CREATE TABLE " + TABLE_ORDER
+                + "(" + ORDER_ID + " INTEGER PRIMARY KEY,"
+                + ORDER_ADDRESS + " TEXT,"
+                + ORDER_BUYER + " INTEGER,"
+                + ORDER_DATE + " DATE,"
+                + ORDER_STATUS + " TINYINT,"
+                + ORDER_SELLER + " INTEGER,"
+                + ORDER_BOOK + " INTEGER,"
+                + " FOREIGN KEY ("+BOOK_SELLER+") REFERENCES "+TABLE1_NAME+"("+T1COL1+"));";
+        sqLiteDatabase.execSQL(query);
+
+        query = "Insert into " + TABLE1_NAME
+                + "("
+                + T1COL2 + ","
+                + T1COL3 + ","
+                + T1COL4 + ","
+                + T1COL5 + ") Values (" +
+                "'admin',12345,'admin@gmail.com','Douglas College')," +
+                "('joe',12345,'joe@gmail.com','Somewhere')";
+
+        sqLiteDatabase.execSQL(query);
+
+
+
+        query = "Insert into " + TABLE_BOOK
+                + "("
+                + BOOK_AUTHOR + ","
+                + BOOK_DESC + ","
+                + BOOK_SELLER + ","
+                + BOOK_PRICE + ","
+                + BOOK_TITLE + ","
+                + BOOK_FOR_SALE + ","
+                + BOOK_IMAGE +
+                ") Values (" +
+                "'Tony Faggioli','As our story unfolds, it becomes clear that there is hell, and then there is hell on earth.',1,18.99,'A Million to One: Beyond'," +
+                "1, 'bookcover2'),('Jennifer L. Armentrout','Samantha is a stranger in her own life.',1,8.99,'Don''t Look Back',1, 'bookcover1')";
+
+        sqLiteDatabase.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE1_NAME);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_BOOK);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_ORDER);
         onCreate(sqLiteDatabase);
-    }
-
-    public void insertFakeBooks(SQLiteDatabase sqLiteDatabase){
-        sqLiteDatabase = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(BOOK_AUTHOR, "Tony Faggioli");
-        values.put(BOOK_DESC, "As our story unfolds, it becomes clear that there is hell, and then there is hell on earth.\n" +
-                "And worst of all...there are some demons you never see coming.\n" +
-                "\n" +
-                "Detective Napoleon Villa’s latest case is spiraling out of control. Drawing on his skills, he battles to save former suspect Kyle Fasano from becoming the next victim.");
-        values.put(BOOK_SELLER, 1);
-        values.put(BOOK_PRICE, 18.99);
-        values.put(BOOK_TITLE, "A Million to One: Beyond");
-        values.put(BOOK_FOR_SALE, 1);
-        values.put(BOOK_IMAGE, "bookcover2");
-        sqLiteDatabase.insert(TABLE_BOOK,null,values);
-        ContentValues values1 = new ContentValues();
-        values1.put(BOOK_AUTHOR, "Jennifer L. Armentrout");
-        values1.put(BOOK_DESC, "Samantha is a stranger in her own life. Until the night she disappeared with her best friend, Cassie, everyone said Sam had it all - popularity, wealth, and a dream boyfriend.\n" +
-                "\n" +
-                "Sam has resurfaced, but she has no recollection of who she was or what happened to her that night. As she tries to piece together her life from before, she realizes it's one she no longer wants any part of. The old Sam took \"mean girl\" to a whole new level, and it's clear she and Cassie were more like best enemies. Sam is pretty sure that losing her memories is like winning the lottery. She's getting a second chance at being a better daughter, sister, and friend, and she's falling hard for Carson Ortiz, a boy who has always looked out for her-even if the old Sam treated him like trash.");
-        values1.put(BOOK_SELLER, 1);
-        values1.put(BOOK_PRICE, 8.99);
-        values1.put(BOOK_TITLE, "Don't Look Back");
-        values1.put(BOOK_FOR_SALE, 1);
-        values1.put(BOOK_IMAGE, "bookcover1");
-        sqLiteDatabase.insert(TABLE_BOOK,null,values1);
     }
 
     public boolean insertUser(String username,String password,String email,String address){
@@ -174,7 +175,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put(BOOK_AUTHOR, author);
         values.put(BOOK_DESC, desc);
         values.put(BOOK_SELLER, sellerId);
-        values.put(BOOK_PRICE, price);
+        values.put(BOOK_PRICE, String.format("%.3f", price));
         values.put(BOOK_TITLE, title);
         values.put(BOOK_FOR_SALE, isForSale);
         Random rand = new Random();
@@ -182,7 +183,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         int int_random = rand.nextInt(upperbound) + 1;
 
         values.put(BOOK_IMAGE, "bookcover" + int_random);
-        long l = sqLiteDatabase.insert(TABLE_BOOK,null,values);
+        long l = sqLiteDatabase.insert(TABLE_BOOK,null, values);
         return l > 0;
     }
 
@@ -212,6 +213,48 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put(T1COL4,userEmail);
         int u = sqLiteDatabase.update(TABLE1_NAME,values,"id=?",
                 new String[]{Integer.toString(userId)});
+        return u > 0;
+    }
+
+    public Cursor getOrders(){
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_ORDER;
+        return sqLiteDatabase.rawQuery(query,null);
+    }
+
+    public Cursor getOrderBySellerId(int sellerId) {
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_ORDER + " WHERE SellerId='" + sellerId + "'";
+        return sqLiteDatabase.rawQuery(query, null);
+    }
+
+    public Cursor getOrderByBuyerId(int buyerId) {
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_ORDER + " WHERE BuyerId='" + buyerId + "'";
+        return sqLiteDatabase.rawQuery(query, null);
+    }
+
+    public boolean insertOrder(int bookId,int buyerId,String address, int sellerId){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(ORDER_ADDRESS, address);
+        values.put(ORDER_BUYER, buyerId);
+        values.put(ORDER_SELLER, sellerId);
+        values.put(ORDER_STATUS, false);
+        values.put(ORDER_BOOK, bookId);
+        values.put(ORDER_DATE, new SimpleDateFormat("YYYY-MM-dd").format(Calendar.getInstance().getTime()));
+
+        long l = sqLiteDatabase.insert(TABLE_ORDER,null, values);
+        return l > 0;
+    }
+
+    public boolean markAsDelivered(int orderId){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(ORDER_STATUS, true);
+        int u = sqLiteDatabase.update(TABLE_ORDER,values,"id=?",
+                new String[]{Integer.toString(orderId)});
+
         return u > 0;
     }
 }
