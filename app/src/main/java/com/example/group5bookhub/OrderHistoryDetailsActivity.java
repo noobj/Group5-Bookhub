360abf8450779c8a1dc2186d625c59d400274b39
gMethmini <gayali.9812@gmail.com>
2024-04-01 01:57:40 -0700

    chore:updated edit order activities for buyer and seller

--- app/src/main/java/com/example/group5bookhub/OrderHistoryDetailsActivity.java
+++ app/src/main/java/com/example/group5bookhub/OrderHistoryDetailsActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -18,12 +21,13 @@ import org.w3c.dom.Text;
 
 public class OrderHistoryDetailsActivity extends AppCompatActivity {
 
+    DatabaseHelper databaseHelper;
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_history_details);
-        Intent intent = getIntent();
-
 
         TextView orderBookName = findViewById(R.id.tvOrdHisBookName);
         TextView orderStatus = findViewById(R.id.tvOrHiStatus);
@@ -33,27 +37,99 @@ public class OrderHistoryDetailsActivity extends AppCompatActivity {
         Button editButton = findViewById(R.id.btnEditOrdHis);
         ImageView orderBookImg = findViewById(R.id.imgVbook);
 
-        int orderId = intent.getIntExtra("ORDER_ID", -1);
-        String bookTitle = intent.getStringExtra("BOOK_TITLE");
-        int bookImage = intent.getIntExtra("BOOK_IMAGE", -1);
-        String bookPrice = intent.getStringExtra("BOOK_PRICE");
-        orderBookName.setText(bookTitle);
-        orderPrice.setText(bookPrice);
-        orderBookImg.setImageResource(bookImage);
+        Intent intent = getIntent();
 
+        if (intent != null) {
+            int orderId = intent.getIntExtra("ORDER_ID", -1);
+            String bookTitle = intent.getStringExtra("BOOK_TITLE");
+            int bookImage = intent.getIntExtra("BOOK_IMAGE", -1);
+            String bookPrice = intent.getStringExtra("BOOK_PRICE");
 
-//        orderBookName.setText("Don't Look Back");
-//        orderStatus.setText("In Transit");
-//        orderAddress.setText("28055 150 Blakely Avenue New Westminister");
-//        orderDate.setText("29/01/2024");
-//        orderPrice.setText("$40");
+            //Retrieve order details from db
+            databaseHelper = new DatabaseHelper(this);
+            Cursor cursor = databaseHelper.getOrderById(orderId);
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    int index = cursor.getColumnIndex(DatabaseHelper.ORDER_STATUS);
+                    int status = cursor.getInt(index);
+                    index = cursor.getColumnIndex(DatabaseHelper.ORDER_ADDRESS);
+                    String address = cursor.getString(index);
+                    index = cursor.getColumnIndex(DatabaseHelper.ORDER_DATE);
+                    String oDate = cursor.getString(index);
+                    index = cursor.getColumnIndex(DatabaseHelper.ORDER_BUYER);
+                    int buyerId = cursor.getInt(index);
+                    index = cursor.getColumnIndex(DatabaseHelper.ORDER_SELLER);
+                    int sellerId = cursor.getInt(index);
 
-        editButton.setOnClickListener(new View.OnClickListener() {
+                    //set status as in transit or delivered
+                    String statusValue;
+                    if (status == 1){
+                        statusValue = "Delivered";
+                        editButton.setVisibility(View.INVISIBLE);
+                    } else {
+                        statusValue = "In Transit";
+                    }
+
+                    //Set retrieved book details and order details to view
+                    orderBookName.setText(bookTitle);
+                    orderStatus.setText(statusValue);
+                    orderAddress.setText(address);
+                    orderDate.setText(oDate);
+                    orderPrice.setText(bookPrice);
+                    orderBookImg.setImageResource(bookImage);
+                    orderBookImg.setTag(bookImage); // Set the tag as the resource ID
+
+                    //Retrieve userId from SharedPreferences
+                    sharedPreferences = getSharedPreferences("UserData", MODE_PRIVATE);
+                    //set default value when userId not found
+                    int userId = sharedPreferences.getInt("userId", -1);
+
+                    editButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            //Check if userId is equal to sellerId
+                            if (userId == sellerId) {
+                                //navigate to edit screen for seller
+                                Intent intent = new Intent(OrderHistoryDetailsActivity.this, EditOrderDetailsSellerActivity.class);
+
+                                //pass the order details
+                                intent.putExtra("orderId",orderId);
+                                intent.putExtra("bookName", orderBookName.getText().toString());
+                                intent.putExtra("address", orderAddress.getText().toString());
+                                // Pass the resource ID of the image if available
+                                int resourceId = (int) orderBookImg.getTag();
+                                intent.putExtra("bookImage", resourceId);
+
+                                startActivity(intent);
+                            }
+                            //Check if userId is equal to buyerId
+                            else if (userId == buyerId) {
+                                Intent intent = new Intent(OrderHistoryDetailsActivity.this, EditOrderDetailsBuyerActivity.class);
+
+                                //pass the order details
+                                intent.putExtra("orderId",orderId);
+                                intent.putExtra("bookName", orderBookName.getText().toString());
+                                intent.putExtra("address", orderAddress.getText().toString());
+                                // Pass the resource ID of the image if available
+                                int resourceId = (int) orderBookImg.getTag();
+                                intent.putExtra("bookImage", resourceId);
+
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                    cursor.close();
+                } while (cursor.moveToNext());
+                cursor.close();
+            }
+        }
+
+        /* editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(OrderHistoryDetailsActivity.this,EditOrderDetailsBuyerActivity.class));
             }
-        });
+        });*/
 
         // Bottom navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
